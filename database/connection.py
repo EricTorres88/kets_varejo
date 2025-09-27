59373e0454cef948dac7889179a6771c42934821
Eric Torres <ericgamertec8@gmail.com>
2025-09-26 21:36:56 -0300

    Ajuste de conexão

--- database/connection.py
+++ database/connection.py
@@ -1,19 +1,30 @@
 import mysql.connector
 from mysql.connector import Error
 from dotenv import load_dotenv
+import streamlit as st
 import os
 
 load_dotenv()
 
 def conectar():
     try:
-        conexao = mysql.connector.connect(
-            host=os.getenv("DB_HOST"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            database=os.getenv("DB_NAME"),
-            port=os.getenv("DB_PORT")
-        )
+        if hasattr(st, "secrets") and "DB_HOST" in st.secrets:
+            conexao = mysql.connector.connect(
+                host=os.getenv("DB_HOST"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASSWORD"),
+                database=os.getenv("DB_NAME"),
+                port=os.getenv("DB_PORT")
+            )
+        else:
+            # Se estiver rodando localmente, usa variáveis do .env
+            conexao = mysql.connector.connect(
+                host=os.getenv("DB_HOST"),
+                user=os.getenv("DB_USER"),
+                password=os.getenv("DB_PASSWORD"),
+                database=os.getenv("DB_NAME"),
+                port=os.getenv("DB_PORT")
+            )
 
         if conexao.is_connected():
             return conexao
